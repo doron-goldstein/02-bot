6834908bdaab53dd05e08cdae47f282c51bc955c
BlackSynder <frogermanz@gmail.com>
2018-02-24 23:33:48 +0200

    make the next command much much nicer

--- cogs/franxx.py
+++ cogs/franxx.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 import discord
 import pytz
@@ -11,7 +11,7 @@ class FranXX:
         self.bot = bot
         self.trans = Translator()
 
-    async def get_next_weekday(self, startdate, day):
+    def get_next_weekday(self, startdate, day):
         days = {
             "Monday": 0,
             "Tuesday": 1,
@@ -22,18 +22,18 @@ class FranXX:
             "Sunday": 6
         }
         weekday = days[day]
-        d = datetime.strptime(startdate, '%Y-%m-%d')
-        t = timedelta((7 + weekday - d.weekday()) % 7)
-        return (d + t).strftime('%Y-%m-%d')
+        days = timedelta((7 + weekday - startdate.weekday()) % 7)
+        return startdate + days
 
-    async def get_remaining_time(self):
+    def get_remaining_time(self):
         day = "Saturday"
-        hour = "23:30"
-        jp_time = datetime.now(pytz.timezone("Japan"))
-        air_date = await self.get_next_weekday(jp_time.strftime('%Y-%m-%d'), day)
-        time_now = jp_time.replace(tzinfo=None)
-        show_airs = datetime.strptime(f'{air_date} - {hour.strip()}', '%Y-%m-%d - %H:%M')
+        hour, minute = "23:30".split(":")
+
+        time_now = datetime.now(pytz.timezone("Japan")).replace(tzinfo=None)
+        air_date = self.get_next_weekday(time_now, day)
+        show_airs = air_date.replace(hour=int(hour), minute=int(minute))
         remaining = show_airs - time_now
+
         if remaining.days < 0:
             return f'{6} Days {remaining.seconds // 3600} Hours and {(remaining.seconds // 60)%60} Minutes.'
         else:
@@ -44,7 +44,7 @@ class FranXX:
     @commands.command(aliases=["episode", "nextepisode", "airtime"])
     async def next(self, ctx):
         """Countdown to next episode of the anime."""
-        remaining = await self.get_remaining_time()
+        remaining = self.get_remaining_time()
         embed = discord.Embed(title="Darling in the FranXX", color=0x0066CC)
         embed.add_field(name="Next Episode", value=remaining)
         embed.set_footer(text='Hype Up Bois')
