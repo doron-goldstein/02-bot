a5b92899fc8effea03723d85c92a8006c03a9a09
BlackSynder <frogermanz@gmail.com>
2018-04-01 00:33:45 +0300

    add pentecost

--- cogs/lirpa.py
+++ cogs/lirpa.py
@@ -24,6 +24,15 @@ class Roles:
             'warframe': 428196015938732034
         }
 
+    @commands.has_permissions(kick_members=True, ban_members=True)
+    @commands.command()
+    async def pentecost(self, ctx):
+        await ctx.send("""One: don't you ever ping me again.
+
+Two: don't you ever touch me again.
+
+Now you have no idea who the hell I am, of which anime Discord I've come from, and I'm not about to tell you my extensive list of nicknames. All I need to be to you and every Ranger on this Discord is a ban hammer. The last mod standing. I do not need your >pat or your >sync. All I need is you to read rules and your discussion skills. And if I can't get that, then you can go back to the DarliFranXX Discord where I found you crawling on. Do I make myself clear? Good.""")
+
     async def on_raw_reaction_add(self, emoji, message_id, channel_id, user_id):
         guild = self.bot.get_channel(channel_id).guild
         member = guild.get_member(user_id)
