05059d954801df468661cfc21f13149bb1f8a5a5
BlackSynder <frogermanz@gmail.com>
2018-04-15 20:40:41 +0300

    reeeeeeeeeeee

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ class ZeroTwo(commands.Bot):
             self.pool = await asyncpg.create_pool(db, ssl=ssl.SSLContext(), loop=self.loop)
 
         if self.redis is None:
-            self.redis = await aioredis.create_redis_pool("redis://" + redis[0], password=redis[1], minsize=5, maxsize=10, pool=self.pool)
+            self.redis = await aioredis.create_redis_pool("redis://" + redis[0], password=redis[1], minsize=5, maxsize=10, loop=self.pool)
 
         if self.session is None:
             self.session = aiohttp.ClientSession()
